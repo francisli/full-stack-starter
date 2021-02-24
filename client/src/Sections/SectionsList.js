794c48e33b36a09a256c36123d87b6b1d7854455
Francis Li <mail@francisli.com>
2021-02-23 20:47:24 -0800

    Section edit/update and delete

--- client/src/Sections/SectionsList.js
+++ client/src/Sections/SectionsList.js
@@ -1,14 +1,26 @@
-import {useEffect, useState} from 'react';
-import {Link} from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 
 import Api from '../Api';
 
 function SectionsList() {
   const [sections, setSections] = useState([]);
 
-  useEffect(function() {
+  useEffect(function () {
     Api.sections.index().then(response => setSections(response.data));
   }, []);
+
+  function onDelete(section) {
+    if (window.confirm(`Are you sure you wish to delete ${section.name}?`)) {
+      // we'll execute code to delete the section
+      Api.sections.delete(section.id).then(function() {
+        // we're filtering the sections list, keeping every section that does not 
+        // match the one we're deleting
+        const newSections = sections.filter(s => s.id !== section.id);
+        setSections(newSections);
+      });
+    }
+  }
 
   return (
     <main className="container">
@@ -16,7 +28,10 @@ function SectionsList() {
       <Link className="btn btn-primary" to="/sections/new">New</Link>
       <ul>
         {sections.map(s => (
-          <li>{s.name}, {s.slug}, {s.position}</li>
+          <li>
+            <p><Link to={`/sections/${s.id}/edit`}>{s.name}, {s.slug}, {s.position}</Link></p>
+            <p><button onClick={() => onDelete(s)} type="button" className="btn btn-sm btn-danger">Delete</button></p>
+          </li>
         ))}
       </ul>
     </main>
