b6ba09a4bf7fd610a28426233bad0853ca48245e
Francis Li <mail@francisli.com>
2021-03-10 09:14:45 -0800

    Added a Create API route

--- routes/api/skills.js
+++ routes/api/skills.js
@@ -9,4 +9,20 @@ router.get('/', async function (req, res) {
   res.json(rows);
 });
 
+router.post('/', async function (req, res) {
+  // build a new Skill row in memory from the form data in the body of the request
+  const row = models.Skill.build(req.body);
+  try {
+    // wait for the database to save the new row
+    await row.save();
+    // if successful, return 201 status (CREATED), and the JSON data of the row
+    res.status(201).json(row);
+  } catch (error) {
+    // if the database returned an error, print it to the console
+    console.log(error);
+    // send back the UNPROCESSABLE ENTITY error code and the error message itself
+    res.status(422).json(error);
+  }
+});
+
 module.exports = router;
